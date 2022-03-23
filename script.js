b440b67a81846336041a696d77e653b0d7e887ba
unknown <koyugurasiddhardha@gmail.com>
2022-03-23 22:35:22 +0530

    Fixed minor bugs

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ async function getQuotes() {
 
 // Tweet Quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} -${authorText.text}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} ${authorText.textContent}`;
     window.open(twitterUrl,"_blank");
 }
 
